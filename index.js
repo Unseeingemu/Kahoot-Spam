004f3e5992a3675363112f35aa766a2304a42d0f
Mafia-boss-lvl-420 <67177667+Mafia-boss-lvl-420@users.noreply.github.com>
2020-09-21 19:34:15 -0400

    Err Catch

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const api = require('kahoot.js-updated')
 
+
 /*
 async function sleep(m) {
     return new Promise(resolve => setTimeout(resolve, m));
@@ -8,18 +9,23 @@ async function sleep(m) {
 
 var i;
 var id = 0;
+var errnum = 0;
 class KahootSpam {
     static async spam(pin, name, amout){
-        for(i = 0; i < amout; i++){
+        try{
+            for(i = 0; i < amout; i++){
 
-            id = id + 1
-            var bot = new api;
-            bot.join(pin, `${name}${id}`)
-            bot.on("joined", () => {
-                console.log(`Bot ${name}${id} has successfully joined game ${pin}`)
-            })
+                id = id + 1
+                var bot = new api;
+                bot.join(pin, `${name}${id}`)
+                bot.on("joined", () => {
+                    console.log(`Bot ${name}${id} has successfully joined game ${pin}`)
+                })
+            }
+        } catch (err){
+            errnum = errnum + 1
+            console.log(`Um Something Went Wrong With This Bot\n err num ${errnum}`)
         }
-        
     }
 
     
