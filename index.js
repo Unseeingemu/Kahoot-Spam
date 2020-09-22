7db05cc66ea89aaf1b5127b7cb842a908115aa6e
Mafia-boss-lvl-420 <67177667+Mafia-boss-lvl-420@users.noreply.github.com>
2020-09-21 21:01:11 -0400

    Woah With Answers Now

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const api = require('kahoot.js-updated')
 
+//test
+//const api = require('../t/node_modules/kahoot.js-updated')
 
-/*
+
+
 async function sleep(m) {
     return new Promise(resolve => setTimeout(resolve, m));
 }
-*/
+
 
 var i;
 var id = 0;
 var errnum = 0;
 class KahootSpam {
     static async spam(pin, name, amout){
+        if(!pin) throw new TypeError('Please Give Me A Pin')
+        if(!name) throw new TypeError('Please Give Me A Name')
+        if(!amout) throw new TypeError('Please Give Me An Amout')
         try{
             for(i = 0; i < amout; i++){
 
@@ -24,13 +30,47 @@ class KahootSpam {
             }
         } catch (err){
             errnum = errnum + 1
-            console.log(`Um Something Went Wrong With This Bot\n err num ${errnum}`)
+            throw new TypeError(`Um Something Went Wrong With This Bot\n err num ${errnum}`)
+        }
+    }
+
+    static async spamWithAnswers(pin, name, amout, slee){
+        if(!pin) throw new TypeError('Please Give Me A Pin')
+        if(!name) throw new TypeError('Please Give Me A Name')
+        if(!amout) throw new TypeError('Please Give Me An Amout')
+        if(!slee) throw new TypeError('Please Give Me "true" or "false" if you want to sleep or not')
+        try{
+            for(i = 0; i < amout; i++){
+
+                id = id + 1
+                var bot = new api;
+                bot.join(pin, `${name}${id}`)
+                bot.on("joined", () => {
+                    console.log(`Bot ${name}${id} has successfully joined game ${pin}`)
+                })
+                bot.on("QuestionStart", question => {
+                    if(slee == true){
+                        let time = Math.floor(Math.random() * 10000);
+                        setTimeout(() => {
+                            let answer = Math.floor(Math.random() * 4);
+                            question.answer(answer);
+                        }, time);
+                    }else if(slee == false){
+                        let answer = Math.floor(Math.random() * 4);
+                        question.answer(answer);
+                    }else{
+                        throw new TypeError ('You need to have "true" or "false" for if you want to sleep')
+                    }
+                });
+            }
+        } catch(err){
+            errnum = errnum + 1
+            throw new TypeError (`Um Something Went Wrong With This Bot\n err num ${errnum}`)
         }
     }
 
     
 
-    
 }
 
 
--- readme.md
+++ readme.md
@@ -14,5 +14,28 @@ api.spam(123456, 'Name', 10)
 ```
 > *This WIll Spam The Kahoot 123456 10 Times With The Name 'Name'*
 ---
+## Spam With Answers
+###### Sleep Is If It Pauses Before It Will Answer A Question
+```js
+spamWithAnswers(pin, name, amout, sleep)
+```
+##### Example
+```js
+//With sleep
+const Kahoot-Spam = require('Kahoot-Spam')
+Kahoot-Spam = api
+spamWithAnswers(123456, "Name", 10, true)
+```
+> *This WIll Spam The Kahoot 123456 10 Times With The Name 'Name' With Answering The Questions With Sleep*
+##
+```js
+//Without sleep
+const Kahoot-Spam = require('Kahoot-Spam')
+Kahoot-Spam = api
+spamWithAnswers(123456, "Name", 10, false)
+```
+> *This WIll Spam The Kahoot 123456 10 Times With The Name 'Name' With Answering The Questions Without Sleep*
+---
 ## Repo Stats
+##### You Should Star/Fork it 😃 Plz (Click The Picture)
 [![Repo Stats](https://github-readme-stats.vercel.app/api/pin/?username=Mafia-boss-lvl-420&repo=Kahoot-Spam&theme=dark&show_icons=true)](https://github.com/Mafia-boss-lvl-420/Kahoot-Spam)
